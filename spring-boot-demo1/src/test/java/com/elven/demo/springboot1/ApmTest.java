8640eff4e48de0afc998b23373c4fe81a0c8e237
elveny <elve1984@163.com>
2017-02-28 23:33:56 +0800

    为apm问题提出一些解决方案

--- spring-boot-demo1/src/test/java/com/elven/demo/springboot1/ApmTest.java
+++ spring-boot-demo1/src/test/java/com/elven/demo/springboot1/ApmTest.java
@@ -8,6 +8,8 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.joda.time.DateTime;
 import org.junit.Test;
+import org.zeroturnaround.zip.FileSource;
+import org.zeroturnaround.zip.NameMapper;
 import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.*;
@@ -32,7 +34,25 @@ public class ApmTest {
     @Test
     public void test1(){
 
-        ZipUtil.pack(new File("D:\\temp\\20170228"), new File("D:\\temp\\xxx.zip"));
+//        ZipUtil.pack(new File("D:\\temp\\20170228"), new File("D:\\temp\\xxx.zip"));
+
+        String sourceDir = "/home/elven/下载/temp/20170228";
+        String targetZip = "/home/elven/下载/temp/zip/xxx.zip";
+
+        final String prefix = "Test";
+
+        // 生成压缩打包文件
+        ZipUtil.pack(new File(sourceDir), new File(targetZip), new NameMapper(){
+
+            @Override
+            public String map(String name) {
+                return name.startsWith(prefix) ? name : null;
+            }
+        });
+
+        // 向已有的压缩包中添加文件
+        FileSource fileSource = new FileSource("No00001", new File("/home/elven/下载/temp/20170228/No00001"));
+        ZipUtil.addEntry(new File(targetZip), fileSource);
     }
 
     @Test
@@ -57,5 +77,18 @@ public class ApmTest {
             lines.add(it.next().getName());
         }
         System.out.println(lines);
+    }
+
+    /**
+     * 1、zip文件需要根据资产包产生，一个资产包产生一个zip
+     * 2、同一个资产包一天内多次送给对方的话，需要变化zip文件的序列号，所以zip文件名是有格式的，格式张包含了当天日期和序列号
+     * （1）维护一个map，key为“资产包编号”+“_”+“日期”，value为“序列号 1,2,3...999”（增加到999后自动置为1）
+     *
+     * 3、对账文件filelist需要根据不同的资产包分别生成
+     * 4、remove文件也需要在文件名中带上序列号，支持大于10万条分文件
+     * 5、根据资产包编号讲文件生成到不同的目录
+     */
+    @Test
+    public void test4() {
     }
 }
