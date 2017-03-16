6d918e236011fe1bc83e3342344aab38088a4ad0
elveny <elve1984@163.com>
2017-03-17 02:33:26 +0800

    myBase7 破解

---
+++ test1/src/main/java/com/elven/demo/test/MyBase7.java
@@ -0,0 +1,34 @@
+/**
+ * elven.tech Inc.
+ * Copyright (c) 2017-2026 All Rights Reserved.
+ */
+package com.elven.demo.test;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * @Filename MyBase7.java
+ *
+ * @description
+ *
+ * @Version 1.0
+ *
+ * @author elven
+ * @History
+ *
+ * <li>Author: elven</li>
+ * <li>Date: 17-3-8 下午9:48</li>
+ * <li>Version: 1.0</li>
+ * <li>Content: create</li>
+ */
+public class MyBase7 {
+
+    @Test
+    public void firstUseOn(){
+        long time = new Date().getTime();
+        System.out.println("1487605350");
+        System.out.println(time/1000);
+    }
+}
