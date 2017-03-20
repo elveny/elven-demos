c81cf81ffa6388321fdee24c36313b771d826f06
elveny <elve1984@163.com>
2017-03-21 01:02:30 +0800

    spring batch

--- spring-boot-batch1/src/main/java/com/elven/demo/springboot/batch1/Application.java
+++ spring-boot-batch1/src/main/java/com/elven/demo/springboot/batch1/Application.java
@@ -4,19 +4,8 @@
  */
 package com.elven.demo.springboot.batch1;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author qiusheng.wu
@@ -31,6 +20,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(
+                BatchConfiguration.class, args)));
     }
 }
---
+++ spring-boot-batch1/src/main/java/com/elven/demo/springboot/batch1/BatchConfiguration.java
@@ -0,0 +1,65 @@
+/**
+ * elven.tech Inc.
+ * Copyright (c) 2017-2026 All Rights Reserved.
+ */
+package com.elven.demo.springboot.batch1;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Filename BatchConfiguration.java
+ *
+ * @description
+ *
+ * @Version 1.0
+ *
+ * @author elven
+ * @History
+ *
+ * <li>Author: elven</li>
+ * <li>Date: 17-3-20 下午11:06</li>
+ * <li>Version: 1.0</li>
+ * <li>Content: create</li>
+ */
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration
+public class BatchConfiguration {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Job job(Step step1) throws Exception {
+        return jobBuilderFactory.get("job1")
+                .incrementer(new RunIdIncrementer())
+                .start(step1)
+                .build();
+    }
+}
