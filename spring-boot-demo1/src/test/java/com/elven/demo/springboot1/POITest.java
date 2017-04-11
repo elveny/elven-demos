d97f4fe2f0c3319df00b47f320ca229007614681
qiusheng.wu <qiusheng.wu@msxf.com>
2017-04-11 20:13:57 +0800

    银行列表信息更新

--- spring-boot-demo1/src/test/java/com/elven/demo/springboot1/POITest.java
+++ spring-boot-demo1/src/test/java/com/elven/demo/springboot1/POITest.java
@@ -5,10 +5,15 @@
 package com.elven.demo.springboot1;
 
 import com.elven.demo.springboot1.common.util.POIUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StopWatch;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +29,8 @@ import java.util.Map;
  * <li>Content: create</li>
  */
 public class POITest {
+
+    private static final Logger logger = LoggerFactory.getLogger(POITest.class);
 
     @Test
     public void test1() throws IOException {
@@ -42,5 +49,70 @@ public class POITest {
 
             System.out.println(":::::::::::::::::::::::end::::::::::::::::"+key);
         }
+    }
+
+    @Test
+    public void updateBankInfoFromExcel() throws IOException {
+
+        String excelFile = "classpath:bank_info20170118.xlsx";
+
+        StopWatch stopWatch = new StopWatch("BankInfo");
+        stopWatch.start("加载excel数据源");
+        Map<String, List<String[]>> data = POIUtil.parse(ResourceUtils.getFile(excelFile));
+        stopWatch.stop();
+
+        StringBuilder sqlStringBuilder = new StringBuilder("");
+
+        stopWatch.start("读取excel数据，更新BankInfo数据");
+        for(String key : data.keySet()){
+
+            if("SQL Results".equals(key)){
+                List<String[]> list = data.get(key);
+                for(String[] strs : list){
+
+                    int i = 1;
+                    String unitedBankNo = strs[i++];
+                    String subBankName = strs[i++];
+                    String clearBankNo = strs[i++];
+                    String areaCode = strs[i++];
+                    String areaName = strs[i++];
+                    String parentAreaCode = strs[i++];
+
+                    logger.info("unitedBankNo:{}", unitedBankNo);
+
+                    if(StringUtils.equals(unitedBankNo, "银行行号")){
+                        continue;
+                    }
+
+                    sqlStringBuilder.append(" update bank_info  ")
+                            .append(" set sub_bank_code = '").append(unitedBankNo).append("', ")
+                            .append("     sub_bank_name = '").append(subBankName).append("', ")
+                            .append("     parent_area_code = '").append(parentAreaCode).append("', ")
+                            .append("     bank_area_code = '").append(areaCode).append("', ")
+                            .append("     united_bank_no = '").append(unitedBankNo).append("', ")
+                            .append("     clear_bank_no = '").append(clearBankNo).append("', ")
+                            .append("     area_name = '").append(areaName).append("', ")
+                            .append("     update_time = now() ")
+                            .append(" where united_bank_no = '").append(unitedBankNo).append("'; \n")
+                    ;
+
+                }
+
+            }
+            else {
+                continue;
+            }
+
+        }
+
+        String bank_info_sql = sqlStringBuilder.toString();
+        logger.info("{}", bank_info_sql);
+
+        FileUtils.write(new File("E:\\code\\idea\\elven\\elven-demos\\spring-boot-demo1\\src\\test\\resources\\bank_info.sql"), bank_info_sql, "UTF-8");
+
+        stopWatch.stop();
+
+        logger.info("{}", stopWatch.prettyPrint());
+
     }
 }
