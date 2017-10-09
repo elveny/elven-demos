8ab3baf7f8d84fae995387652ba7c31db414cdfc
qiusheng.wu <qiusheng.wu@msxf.com>
2017-10-09 10:51:11 +0800

    正则表达式测试

--- spring-boot-demo1/src/test/java/com/elven/demo/springboot1/test/regex/RegexTester.java
+++ spring-boot-demo1/src/test/java/com/elven/demo/springboot1/test/regex/RegexTester.java
@@ -182,4 +182,19 @@ public class RegexTester {
         boolean rs = matcher.find();
         System.out.println(rs);
     }
+
+    @Test
+    public void test14(){
+        // 要验证的字符串
+        String str = "宝贝格子（北京）科技(有限)公司";
+        // 邮箱验证规则
+        // 编译正则表达式
+//        Pattern pattern = Pattern.compile("^[\\w\\-\\.\\u00b7\\u2022\\u4E00-\\u9FA5]{1,64}$");
+        Pattern pattern = Pattern.compile("^[\\w\\-\\.\\u00b7\\u2022\\uff08\\uff09\\u4E00-\\u9FA5]{1,64}$");
+
+        Matcher matcher = pattern.matcher(str);
+        // 字符串是否与正则表达式相匹配
+        boolean rs = matcher.find();
+        System.out.println(rs);
+    }
 }
