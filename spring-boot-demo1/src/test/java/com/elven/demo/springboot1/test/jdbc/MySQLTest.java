a5de91d9ac011e4b717bc3694a912f56788124d0
elveny <elve1984@163.com>
2018-01-22 23:16:52 +0800

    jdbc

--- spring-boot-demo1/src/test/java/com/elven/demo/springboot1/test/jdbc/MySQLTest.java
+++ spring-boot-demo1/src/test/java/com/elven/demo/springboot1/test/jdbc/MySQLTest.java
@@ -2,16 +2,14 @@ package com.elven.demo.springboot1.test.jdbc;
 
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MySQLTest {
 
     @Test
     public void test1(){
         String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://::FFFF:192.168.56.1:3306/mysql?user=amdin&password=Admin123!&useUnicode=true&characterEncoding=UTF-8";
+        String url = "jdbc:mysql://192.168.56.1:3306/mysql?useUnicode=true&characterEncoding=UTF-8";
         String username = "admin";
         String password = "Admin123!";
         Connection conn = null;
@@ -19,6 +17,17 @@ public class MySQLTest {
             Class.forName(driver); //classLoader,加载对应驱动
             conn = (Connection) DriverManager.getConnection(url, username, password);
             System.out.println(conn);
+
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from user");
+            System.out.print("Host");
+            System.out.print("\t\t");
+            System.out.println("User");
+            while (resultSet.next()){
+                System.out.print(resultSet.getNString(1));
+                System.out.print("\t\t");
+                System.out.println(resultSet.getNString(2));
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
