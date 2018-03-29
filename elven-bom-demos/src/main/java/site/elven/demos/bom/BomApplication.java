cce2907e6d0f2ba3c4f2ffb032c0aee242d0f425
qiusheng.wu <qiusheng.wu@msxf.com>
2018-03-29 14:16:49 +0800

    netty

---
+++ elven-bom-demos/src/main/java/site/elven/demos/bom/BomApplication.java
@@ -0,0 +1,35 @@
+/**
+ * msxf.com Inc.
+ * Copyright (c) 2018-2026 All Rights Reserved.
+ */
+package site.elven.demos.bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qiusheng.wu
+ * @Filename BomApplication.java
+ * @description
+ * @Version 1.0
+ * @History <li>Author: qiusheng.wu</li>
+ * <li>Date: 2018/3/23 16:28</li>
+ * <li>Version: 1.0</li>
+ * <li>Content: create</li>
+ */
+@SpringBootApplication
+@RestController
+public class BomApplication {
+    @RequestMapping("/home")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BomApplication.class, args);
+    }
+}
