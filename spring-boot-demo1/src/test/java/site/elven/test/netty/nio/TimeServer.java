b449bacce68159a627aecd26c476b5324cad5d9f
elveny <elve1984@163.com>
2018-09-13 22:31:06 +0800

    java nio TimeServer

---
+++ spring-boot-demo1/src/test/java/site/elven/test/netty/nio/TimeServer.java
@@ -0,0 +1,9 @@
+package site.elven.test.netty.nio;
+
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
